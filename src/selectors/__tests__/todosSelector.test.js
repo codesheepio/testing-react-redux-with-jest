1a9713c96cf5265a2d81f7b2450f2b0398f38c41
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-26 20:48:58 +0700

    Remove unnecessary code

--- src/selectors/__tests__/todosSelector.test.js
+++ src/selectors/__tests__/todosSelector.test.js
@@ -8,11 +8,7 @@ describe('todosSelector', () => {
         { id: 2, text: 'Drink coffee', complete: true },
       ],
     }
-    expect(selectTodos(state)).toEqual({
-      todos: [
-        { id: 1, text: 'Feed cat', complete: false },
-        { id: 2, text: 'Drink coffee', complete: true },
-      ],
-    })
+
+    expect(selectTodos(state)).toEqual({ todos: state.todos })
   })
 })
